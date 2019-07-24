cc12bf7a5b9bc835649d358204d2224b7187337d
nameet <nameet.nayak@mavs.uta.edu>
2019-07-24 02:23:49 -0500

    Initial Commit

---
+++ app/src/main/java/HelpAll/org/MainActivity.java
@@ -0,0 +1,28 @@
+package HelpAll.org;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button ButtonLessonId = (Button) findViewById(R.id.buttonLessonId);
+        ButtonLessonId.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText EditTextNumberId = (EditText) findViewById(R.id.editTextNumberId);
+                int num = Integer.parseInt(EditTextNumberId.getText().toString());
+                System.out.println(num);
+            }
+        });
+
+    }
+}
