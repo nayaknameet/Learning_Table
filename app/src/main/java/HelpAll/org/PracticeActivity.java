34ba12b4cc86e99d2f36a3049ab1aea6f4a4f8c9
nameet <nameet.nayak@mavs.uta.edu>
2019-07-31 21:08:35 -0500

    PracticeActivity complete preview layout

---
+++ app/src/main/java/HelpAll/org/PracticeActivity.java
@@ -0,0 +1,13 @@
+package HelpAll.org;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+    }
+}
